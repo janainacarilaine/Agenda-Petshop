0936d938f721941c54423824565b90ab1348bfb2
janainacarilaine <janainacarilaine@gmail.com>
2021-03-17 17:03:03 -0300

    finalizada implementação dos metodos GET, POST, PATCH E DELETE

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,13 +1,30 @@
 const Atendimento = require('../models/atendimento');
 module.exports = app => {
 
-    app.get('/atendimentos', (req, resp) =>{
-        resp.send(`GET -> rota de atendimentos`);
+    app.get('/atendimentos', (req, resp) => {
+        Atendimento.lista(resp);
+    });
+
+    app.get('/atendimentos/:id', (req, resp) => {
+        console.log(req.params.id);
+        const id = parseInt(req.params.id);
+        Atendimento.buscaPorId(id, resp);
     });
 
     app.post('/atendimentos', (req, resp) => {
         const atendimento = req.body;
         Atendimento.adiciona(atendimento, resp);
+    });
+
+    app.patch('/atendimentos/:id',(req, resp) => {
+        const valores = req.body;
+        const id = parseInt(req.params.id);
+        Atendimento.altera(id, valores, resp);
+    });
+
+    app.delete('/atendimentos/:id', (req, resp) => {
+        const id = parseInt(req.params.id);
+        Atendimento.remove(id, resp);
     });
 
 }
--- models/atendimento.js
+++ models/atendimento.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 const conexao = require('../infra/conexao');
 class Atendimento{
+    
     adiciona(atendimento, resp){
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
@@ -37,13 +38,67 @@ class Atendimento{
                 if (erro){
                     resp.status(400).json(erro);
                 } else {
-                    resp.status(201).json(resultados);
+                    resp.status(201).json({atendimento});
                 }
             });
 
         }
         
     }
+
+    lista(resp){
+        const sql = `SELECT * FROM atendimentos`;
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro){
+                resp.status(400).json(erro);
+            } else {
+                resp.status(200).json(resultados);
+            }
+        });
+    }
+
+    buscaPorId(id, resp){
+        const sql = `SELECT * FROM atendimentos WHERE id = ${id}`;
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro){
+                resp.status(400).json(erro);
+            } else {
+                resp.status(200).json(resultados[0]);
+            }
+        });
+
+    }
+
+    altera(id, valores, resp){
+        if (valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        }
+        const sql = `UPDATE atendimentos SET ? WHERE id = ?`
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (erro){
+                resp.status(500).json(erro);
+            } else {
+                resp.status(200).json({id, valores});
+            }
+        });
+    }
+
+    remove(id, resp){
+        const sql = `DELETE FROM atendimentos WHERE id=?`;
+
+        conexao.query(sql, id, (erro, resultado) => {
+            if (erro){
+                resp.status(500).json(erro);
+            } else {
+                resp.status(200).json({'status' : 'removido', 'id' : id});
+            }
+        });
+    }
+
+
 }
 
 module.exports = new Atendimento;
